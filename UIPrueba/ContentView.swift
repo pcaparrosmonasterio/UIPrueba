6ad92a1fd958d83ce3f91692a9b6058aca518427
pcaparrosmonasterio <pcaparrosmonasterio@gmail.com>
2025-11-19 16:23:45 +0100

    Spanding View

--- UIPrueba/ContentView.swift
+++ UIPrueba/ContentView.swift
@@ -9,14 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, swift!")
-            Text("hola")
-        }
-        .padding()
+        VStack{
+            Text("🥖🥖🥖").font(Font.largeTitle)
+            Text("89").font(Font.largeTitle).kerning(-1).fontWeight(.bold)
+            Slider(
+                value: .constant(50),
+                    in: 1...100,
+                    step: 5
+                ) {
+                    Text("Speed")
+                } minimumValueLabel: {
+                    Text("1").fontWeight(.bold)
+                } maximumValueLabel: {
+                    Text("100").fontWeight(.bold)
+                }
+            Button("TRY"){
+                print("trying")
+            }.font(Font.title3)
+                .padding()
+                .foregroundColor(.white)
+                .background(.blue)
+                .cornerRadius(21)
+                
+        }.padding()
+        
     }
 }
 
